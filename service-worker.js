29d4c8b6f4aa2b0e37a46548a565c21198ec165c
FROGDOP <gui.jacquot@live.fr>
2026-07-22 01:39:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,26 @@
-const cacheName = 'camlog-v1';
+const cacheName = 'camlog-v2';
 const appFiles = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg', './ma-police.otf'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(appFiles)));
   self.skipWaiting();
 });
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key.startsWith('camlog-') && key !== cacheName).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
